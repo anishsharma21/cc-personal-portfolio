b14ca6668cb78fd7cdc12a3ef771261fa414c833
anishsharma21 <anisharma2103@gmail.com>
2023-12-21 17:20:50 +1100

    Add scroll-to-top button to index.html and index.js, and style the button in styles.css

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// Get the button
+var mybutton = document.getElementById("scrollTopButton");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 
